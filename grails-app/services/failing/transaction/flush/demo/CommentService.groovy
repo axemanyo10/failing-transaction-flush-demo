c0e13af576e583753a091b844a082ed812e7514a
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2018-10-24 16:19:17 +0100

    Refactored service to do all validating before transaction
    I thought that if the transaction only saved valid
    domain class records then it would flush and commit, but
    it doesn't. It looks like if any calls to validate() return
    false then any future transactions are doomed to not
    flush properly.

--- grails-app/services/failing/transaction/flush/demo/CommentService.groovy
+++ grails-app/services/failing/transaction/flush/demo/CommentService.groovy
@@ -4,13 +4,26 @@ import grails.gorm.transactions.Transactional
 
 class CommentService {
 
-    @Transactional
     void addComments(List<String> inputs) {
+        List<Comment> validComments = validCommentsFromInputs(inputs)
+        saveComments(validComments)
+    }
+
+    List<Comment> validCommentsFromInputs(List<String> inputs) {
+        List<Comment> validComments = []
         inputs.each { input ->
             Comment comment = new Comment(comment: input)
             if (comment.validate()) {
-                comment.save()
+                validComments << comment
             }
+        }
+        validComments
+    }
+
+    @Transactional
+    void saveComments(List<Comment> comments) {
+        comments.each { comment ->
+            comment.save()
         }
     }
 
